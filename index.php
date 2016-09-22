d2fb917730fe0260397c8fac6ec1317e98007e8a
Jer Lance <me@jerlance.com>
2016-09-21 17:23:09 -0700

    Adds an inserter, some logging, and swap files to the git ignore

--- index.php
+++ index.php
@@ -22,14 +22,24 @@ if ($redirectUrl) {
 
 
 function retrieveRedirectUrl($key, $dbInfo) {
-    $handle = mysqli_connect($dbInfo['host'], $dbInfo['login'], $dbInfo['pass'], $dbInfo['database'], 3306, '/home/jer_/mysqld.sock') 
-        or die('Error connecting to database: ' . mysqli_connect_error());
+    // Assumes a table named 'redirects' with column 'redirect_key' as the short
+    // form and 'redirect_url' as the long form.
+
+    $handle = mysqli_connect($dbInfo['host'], $dbInfo['login'], $dbInfo['pass'], $dbInfo['database']) 
+        or die('Error connecting to database');
     
     $query = "SELECT redirect_url FROM redirects WHERE redirect_key = '$key'";
     $resultArr = array();
 
     $result = mysqli_query($handle, $query);
     $row    = mysqli_fetch_assoc($result);
+
+    $counter = "UPDATE redirects SET uses = uses + 1 WHERE redirect_key = '$key'";
+    $logger  = "INSERT INTO redirect_log VALUES('$key', NOW())";
+    mysqli_query($handle, $counter);
+    mysqli_query($handle, $logger);
+
+    mysqli_close($handle);
 
     if (isset($row['redirect_url'])) {
         return $row['redirect_url'];
@@ -39,4 +49,39 @@ function retrieveRedirectUrl($key, $dbInfo) {
 
 
 ?>
-<h1>Url Redirector</h1>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <title>Jer...WTF Redirection Service</title>
+    <style>
+        body {
+            background: lightgrey;
+        }
+        
+    </style>
+</head>
+<body>
+<h1>Jer...WTF?!</h1>
+<h2>Redirection Service</h2>
+
+<form id="urlshortenerform" method="post" action="https://jer.wtf/url_insert.php">
+<input type="text" id="shortname" name="shortname" />Short Name (<span onclick="makeShort()">Generate</span>)<br />
+<input type="url" id="url" name="url" />URL <br />
+<input type="text" id="secusr" name="secusr" />Username <br />
+<input type="password" id="seckey" name="seckey" />Password <br />
+<input type="submit" /><br />
+</form>
+</body>
+<script>
+function makeShort()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 7; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    document.getElementById('shortname').value = text;
+}
+</script>
+</html>
---
+++ url_insert.php
@@ -0,0 +1,40 @@
+<?php
+
+$urlShort = $_POST['shortname'];
+$urlLong  = $_POST['url'];
+$username = $_POST['secusr'];
+$password = $_POST['seckey'];
+$errors   = null;
+
+
+// TODO: once you leave POC state, let's do this intelligently
+// Assumes a variable called $security as a 'username'=>'password' array
+require('../configs/jer.wtf.users.txt');
+
+require('../configs/db.conf');
+
+if (!isset($security[$username]) || $security[$username] != $password) {
+    $errors .= "Authorization Failure.\n";        
+}
+
+$shortenedChars = '/^[A-za-z0-9_]{1,20}$/';
+if (!preg_match($shortenedChars, $urlShort)) {
+    $errors .= "Invalid short name.\n";
+}
+
+if (filter_var($urlLong, FILTER_VALIDATE_URL) === false) {
+    $errors .= "Invalid url.\n";
+}
+
+if (!$errors) {
+    $handle = mysqli_connect($jerwtf['host'], $jerwtf['login'], $jerwtf['pass'], $jerwtf['database'])
+        or die('Failure connecting to database');
+    $query = "INSERT INTO redirects VALUES('$urlShort', '$urlLong', NOW())";
+    mysqli_query($handle, $query) or die('Failure writing to database');
+    mysqli_close($handle);
+    echo 'Success! <a href="jer.wtf/' . $urlShort . '">jer.wtf/' . $urlShort . '</a>';
+} else {
+    echo 'Fail. Errors:';
+    echo $errors;
+}
+
