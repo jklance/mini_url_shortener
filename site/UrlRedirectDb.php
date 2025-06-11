8f89fb196233f36117b9a4d5a3fd03777158e062
Jer <gitkraken@ibloviate.org>
2025-06-11 15:49:49 -0400

    or not

--- site/UrlRedirectDb.php
+++ site/UrlRedirectDb.php
@@ -118,7 +118,7 @@ class UrlRedirectDb
         $query  = "INSERT INTO redirects VALUES";
         $query .= "('" . $redirector->getShort() . "','" . $redirector->getLong() . "',NOW(),0,'" . $redirector->getUser() ."')";
 
-        if (mysqli_query(mysql: $this->_dbHandle, query: $query) === true) {
+        if (mysqli_query($this->_dbHandle, $query) === true) {
             return true;
         }
         return false;
@@ -138,9 +138,9 @@ class UrlRedirectDb
             $query .= " LIMIT $count";
         }
 
-        $result = mysqli_query(mysql: $this->_dbHandle, query: $query);
+        $result = mysqli_query($this->_dbHandle, $query);
         if ($result) {
-            $resArr = mysqli_fetch_all(result: $result, mode: MYSQLI_ASSOC);
+            $resArr = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
             if (is_array($resArr)) {
                 return $resArr;
@@ -161,9 +161,9 @@ class UrlRedirectDb
             $query .= " LIMIT $count";
         }
 
-        $result = mysqli_query(mysql: $this->_dbHandle, query: $query);
+        $result = mysqli_query($this->_dbHandle, $query);
         if ($result) {
-            $resArr = mysqli_fetch_all(result: $result, mode: MYSQLI_ASSOC);
+            $resArr = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
             if (is_array($resArr)) {
                 return $resArr;
@@ -185,11 +185,11 @@ class UrlRedirectDb
             $query .= " LIMIT $count";
         }
 
-        $result = mysqli_query(mysql: $this->_dbHandle, query: $query);
+        $result = mysqli_query($this->_dbHandle, $query);
         if ($result) {
-            $resArr = mysqli_fetch_all(result: $result, mode: MYSQLI_ASSOC);
+            $resArr = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-            if (is_array(value: $resArr)) {
+            if (is_array($resArr)) {
                 return $resArr;
             }
         }
@@ -200,8 +200,8 @@ class UrlRedirectDb
     private function _retrieveRedirectUrlFromDb($abbreviation) {
         $query = "SELECT redirect_url FROM redirects WHERE redirect_key = '$abbreviation'";
 
-        $result = mysqli_query(mysql: $this->_dbHandle, query: $query);
-        $row    = mysqli_fetch_assoc(result: $result);
+        $result = mysqli_query($this->_dbHandle, $query);
+        $row    = mysqli_fetch_assoc($result);
 
         if (isset($row[redirect_url])) {
             return $row[redirect_url];
@@ -212,7 +212,7 @@ class UrlRedirectDb
     private function _updateDbLogForRedirect($abbreviation) {
         $query  = "INSERT INTO redirect_log VALUES('$abbreviation', NOW())";
         
-        if (mysqli_query(mysql: $this->_dbHandle, query: $query) === true) {
+        if (mysqli_query($this->_dbHandle, $query) === true) {
             return true;
         }
 
@@ -229,11 +229,11 @@ class UrlRedirectDb
     private function _openHandle() {
         if ($this->_fieldsFilled()) {
             $this->_dbHandle = mysqli_connect(
-                hostname: $this->_hostname,
-                username: $this->_username,
-                password: $this->_password,
-                database: $this->_database,
-                port: $this->_portnum
+                $this->_hostname,
+                $this->_username,
+                $this->_password,
+                $this->_database,
+                $this->_portnum
             ) or die('Graceless DB failure connecting!');
             return true;
         }
@@ -241,6 +241,6 @@ class UrlRedirectDb
     }
 
     private function _closeHandle() {
-        mysqli_close(mysql: $this->_dbHandle);
+        mysqli_close($this->_dbHandle);
     }
 }
